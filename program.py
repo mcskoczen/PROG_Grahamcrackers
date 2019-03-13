63720d84bf3d29c07f9bea6f302079e31312d18e
A-Ligman <48067546+A-Ligman@users.noreply.github.com>
2019-03-13 14:49:46 -0400

    Add files via upload

--- program.py
+++ program.py
@@ -22,30 +22,57 @@ from scipy import stats
 import matplotlib.pyplot as plt
 import matplotlib.font_manager
 import pandas as pd
+import math
 
 class Computer:
- def __init__(self, branch, host, cpu_total):
+ def __init__(self, branch, host, cpu_total, HBOS):
     self.branch = branch
     self.host = host
     self.cpu_total = cpu_total
+    self.HBOS = HBOS
 
 d = {}
+topten= {}
 for x in range(3):
   df = pd.read_csv("cpu" + str(x) + ".csv")
   
   d[x] = df
 #creating a loop to read files and assign them to dataframes with a value of x where x is the number of files
 
+#var0 = cpu_total
 
 listOfWorkstations = []
 
 length = len(df)
-for index in range(length):
-    listOfWorkstations.append(Computer(df.loc[index].enrichment_branch_name, df.loc[index].beat_hostname, df.loc[index].system_cpu_total_pct))
+N = int(math.sqrt(length - 1)) 
+w = length % N
+if w > 0:
+  N = N + 1
 
+# N is the square root of the number of workstations, this is the variable to be changed based on the number of workstations when being scaled up for larger production
+for index in range(length):
+    listOfWorkstations.append(Computer(df.loc[index].enrichment_branch_name, df.loc[index].beat_hostname, df.loc[index].system_cpu_total_pct, HBOS = 0))
+
+numberofvariables = 1
+#create a thing where we call a number to get a string where string is the names of variables
+for stuff in range(numberofvariables):
+  #variable[stuff] -> math thingies
+  #sort by value
+  for x in range(13):
+    #object[x].HBOS =  object[x].HBOS + log10(N*(object[12].var+str(stuff) - object[0].variablename))
+    pass
+  for x in range(13,26):
+    #object[x].HBOS = object[x].HBOS + log10(N*(object[25].variablename - object[13].variablename))
+    pass
+  for x in range(26,39):
+    #object[x].variablename = log 
+    pass 
+  pass
 
 outliers_fraction = 10/162 
 
 print(d[0])
 print(d[1])
 print(d[2])
+
+print(d[0].system_cpu_system_pct)
