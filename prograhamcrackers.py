ad2e4581595c602053f42f43ca83fbdd0784b48c
A-Ligman <48067546+A-Ligman@users.noreply.github.com>
2019-03-27 14:56:18 -0400

    Add files via upload

--- prograhamcrackers.py
+++ prograhamcrackers.py
@@ -39,11 +39,16 @@ for num in range(1):
 
   for varindex in range(4, numberofcolumns):
     
-    df.sort_values(by = nameslist[varindex])
+    df.sort_values(by = nameslist[varindex], ascending = True)
     #sorting the dataframe by the values of the dataframe
 
     for x in range(length):
-     a = int(x/N) + 1
+     a = int(x/N)
+
+     if x % N != 0:
+       a + 1
+     if x == 0:
+       a + 1
      #determining which bin we're in
      
      b = a * N
@@ -53,22 +58,32 @@ for num in range(1):
      #setting the beginning of the bin
 
 
-     if (b == N*N):
-       b = length
-       c = N * (N - 1)
+     bad = N * N
+     if (b == bad):
+       b = length -1
+       c = length % N
+       c = length - c
       #in order to account for the last bin being too short, we make sure c and t are both the correct value for the final bin
      
      
      g = df.iat[b, varindex]
      j = df.iat[c, varindex]
+     k = g - j
 
-     z = df.at[x, "HBOS"] + math.log10(N*(g - j))
+     if k <= 0 :
+       print(g)
+       print(j)
+     
+     while k == 0:
+       b = b + 1
+       g = df.iat[b, varindex]
+       k = g - j
+
+     z = df.at[x, "HBOS"] + math.log10(k/N)
      #doing the math to find the HBOS vallue by takin the base 10 logarithm of N times the difference of the values 
      #of the end of the bin and the beginning of the bin and adding it to the previous value of the HBOS algorithm
          
      df.at[x, "HBOS"] = z
-
-  print()
 
   print("This is for the time interval " + timestamp)
   #df.to_csv('HBOS_topten_fortime' + timestamp + '.csv')
