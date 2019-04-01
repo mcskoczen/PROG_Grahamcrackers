6a02d2a05fbfa5a53bd0694398fde40a81e612fe
A-Ligman <48067546+A-Ligman@users.noreply.github.com>
2019-04-01 14:42:38 -0400

    Add files via upload

--- prograhamcrackers.py
+++ prograhamcrackers.py
@@ -18,8 +18,7 @@ for file_index in range(1):
   timestamp = "between " + str(minimum) + " and " + str(maximum)
 
   row_count = len(df)
-  print("The number of rows in df is: ")
-  print(row_count)
+  print("The number of rows in df is: " + str(row_count))
 
   N = int(math.sqrt(row_count)) 
   # N is the square root of the number of workstations, this is the variable to be changed based on the number of workstations when being scaled up for larger production
@@ -47,7 +46,7 @@ for file_index in range(1):
     #sorting the dataframe by the values of the variable in ascending order
     bin_start = 0
     bin_end = N - 1
-
+    
     for x in range(row_count):
      if df.iat[x, var_index] == 0:
        bin_start = x + 1
@@ -73,15 +72,23 @@ for file_index in range(1):
      k = g - j
      if k == 0:
        print("The problem bin is in the column labelled " + list_of_columns[var_index])
-      #  print("The problem bin starts at workstation " +str(bin_start) + " with a value of " + str(j))
-      #  print("and ends at workstation "+ str(bin_end)+ " with a value of " + str(g))
-       
+       print("It starts at workstation " +str(bin_start) + " with a value of " + str(j))
+      
+
+     while k == 0:
+       bin_end = bin_end + 1
+       g = df.iat[bin_end, var_index]
+       j = df.iat[bin_start, var_index]
+       k = g - j
+       if k != 0:
+         print("and ends at workstation "+ str(bin_end)+ " with a value of " + str(g))    
+     
      z = df.at[x, "HBOS"] + math.log10(k/N)
      #doing the math to find the HBOS vallue by taking the base 10 logarithm of N times the difference of the values 
      #of the end of the bin and the beginning of the bin and adding it to the previous value of the HBOS algorithm
      df.at[x, "HBOS"] = z
 
-  df = df.sort_values(by = "HBOS")
+  df = df.sort_values(by = "HBOS", ascending = False)
   print(df.HBOS)
   print("This is for the time interval " + timestamp)
   #df.to_csv('HBOS_topten_fortime' + timestamp + '.csv')
